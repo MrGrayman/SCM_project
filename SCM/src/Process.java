6328a92119d09a6bdec761aa604f056959f3e539
ssky <guy_ck@hotmail.com>
2019-02-13 02:31:52 +0700

    ProjectB Done

--- SCM/src/Process.java
+++ SCM/src/Process.java
@@ -6,9 +6,16 @@ public class Process {
 		this.product = input.getProduct();
 	}
 	public double calculate(){
-		if(this.product>0&&this.product<=100000)
-			return this.salary+(this.product*0.05);
+		double compensation;
+		if(this.product>0&&this.product<=50000)
+			compensation = this.product*0.05;
 		else 
-			return this.salary+(this.product*0.1);
+			compensation = this.product*0.1;
+		if(this.product>0&&compensation < 20000)
+			compensation = 20000;
+		
+		return this.salary+compensation;
+		
+			
 	}
 }
